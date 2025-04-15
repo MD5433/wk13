4cdf74d4a32e8c8e4d5fb8f3c83c47e7fa97b927
111513 <mariahdungey01@gmail.com>
2025-04-15 14:15:16 -0400

    notes old

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,48 @@
 
 public class Main {
     public static void main(String[] args) {
-        Runnable runnable = new Runnable()
-        {
-            @Override
-            public void run() {
-                String name = Thread.currentThread().getName();
-                int count = 0;
-                while (count < 10)
-                    System.out.println(name + ": " + count++);
-            }
-        };
+        Hi obj1 = new Hi();
+        Hello obj2 = new Hello();
 
-        Thread threadA = new Thread(runnable);
-        Thread threadB = new Thread(runnable);
-        System.out.println("Starting threads");
-        threadA.start();
-        threadB.start();
-        try {
-            threadA.join();
-            threadB.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("End of Main.main()");
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
     }
+
+
 }
+
+
+class Hi implements Runnable{
+
+    public void run(){
+        for (int i = 0; i < 5; i++) {
+            System.out.println("hi");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+
+        }
+    }
+
+
+}
+
+class Hello implements Runnable{
+    public void run(){
+        for (int i = 0; i < 5; i++) {
+            System.out.println("hello");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+
+        }
+    }
+
+
+}
+
